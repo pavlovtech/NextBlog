36e54f7f2a802043acc4874989ad0aeb1c2f88c5
Oleksandr Pavlov <olpavlov@deloitte.com>
2023-05-12 18:56:19 +0300

    up

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,12 +1,9 @@
-import Posts from '@/app/components/posts'
 import Link from 'next/link';
 
 const AdminPage = async () => {
   return <>
     <div className='container'>
-      <Link href='/projects/create' className='bg-green-600 color p-3 text-white'>Create a new project</Link> 
-      {/* @ts-expect-error Server Component */}
-      <Posts /> 
+
     </div>
   </>
 }
