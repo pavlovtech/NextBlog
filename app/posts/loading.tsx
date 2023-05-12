2697531c031cd1371556b9957f6d882a4aa2fc42
Alexander Pavlov <olpavlov@deloitte.com>
2023-05-12 19:59:44 +0300

    up
    jjj

--- app/posts/loading.tsx
+++ app/posts/loading.tsx
@@ -1,6 +1,5 @@
 import { Blinker } from "../components/ui/loading";
 
 export default function Loading() {
-    // You can add any UI inside Loading, including a Skeleton.
-    return <Blinker />;
+    return  <Blinker />;
 }
