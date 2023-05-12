2bb8b45f8c8c0dcd45203cc43329e8511866a190
Oleksandr Pavlov <olpavlov@deloitte.com>
2023-05-12 17:01:59 +0300

    new framework

--- next.config.js
+++ next.config.js
@@ -1,13 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    appDir: true,
-    serverActions: true,
-    serverComponentsExternalPackages: ["mysql2"]
-  }
-}
+// next.config.js
+const { withContentlayer } = require('next-contentlayer')
 
+/** @type {import('next').NextConfig} */
+const nextConfig = { reactStrictMode: true, swcMinify: true }
 const { withAxiom } = require('next-axiom');
 
-module.exports = withAxiom(nextConfig)
+module.exports = withAxiom(withContentlayer(nextConfig));
+
