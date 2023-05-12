1fd0396719b686363c6a523412d1fa90302af11d
Alexander Pavlov <olpavlov@deloitte.com>
2023-05-12 22:04:24 +0300

    up

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,12 @@
 import '../styles/globals.css'
 
-import { Header } from '../components/Header'
+import { Header } from '../components/header'
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html>
       <head>
-        <title>Contentlayer Next.js Example</title>
+        <title>Alex Pavlov - blog</title>
         <link rel="icon" type="image/x-icon" href="/favicon.png" />
       </head>
       <body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import Link from "next/link";
 import { compareDesc, format, parseISO } from "date-fns";
 import { allPosts, Post } from "contentlayer/generated";
-import { getMDXComponent } from "next-contentlayer/hooks";
 
 function PostCard(post: Post) {
   return (
