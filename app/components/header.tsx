c981049fb9093ee1c25e55cbf68e5f649ffda414
Oleksandr Pavlov <olpavlov@deloitte.com>
2023-05-14 13:43:06 +0300

    refactoring

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -3,22 +3,44 @@ import Link from 'next/link'
 import { usePathname } from 'next/navigation';
 import Typewriter from 'typewriter-effect';
 
+
+const headerNavLinks = [
+  { href: '/', title: 'Blog' },
+  { href: '/tags', title: 'Tags' },
+  { href: '/projects', title: 'Projects' },
+  { href: '/about', title: 'About' },
+]
+
 export function Header() {
   const pathname = usePathname();
 
   return (
-    <div>
-      <div className="text-primary-color dark:text-primary-color-dark flex items-center justify-between text-xl font-semibold">
-
-        <Typewriter
-          options={{
-            strings: [`user@alex.pavlov.dev: ~${pathname} `],
-            autoStart: true,
-            loop: false,
-            deleteSpeed: 100000
-          }}
-        />
+    <header className="flex items-center justify-between py-10">
+      <div>
+        <div className="text-primary-color dark:text-primary-color-dark flex items-center justify-between text-xl font-semibold">
+          <Typewriter
+            options={{
+              strings: [`user@alex.pavlov.dev: ~${pathname} `],
+              autoStart: true,
+              loop: false,
+              deleteSpeed: 100000
+            }}
+          />
+        </div>
       </div>
-    </div>
+      <div className="flex items-center text-base leading-5">
+        <div className="hidden sm:block">
+          {headerNavLinks.map((link) => (
+            <Link
+              key={link.title}
+              href={link.href}
+              className="p-1 font-medium text-gray-900 dark:text-gray-100 sm:p-4"
+            >
+              {link.title}
+            </Link>
+          ))}
+        </div>
+      </div>
+    </header>
   )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -12,12 +12,6 @@ export const metadata = {
   description: 'Alex Pavlov - blog about programming'
 }
 
-const headerNavLinks = [
-  { href: '/', title: 'Blog' },
-  { href: '/tags', title: 'Tags' },
-  { href: '/projects', title: 'Projects' },
-  { href: '/about', title: 'About' },
-]
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -29,26 +23,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body className={`${inter.className} bg-white text-black antialiased dark:bg-background-color dark:text-white`}>
         <div className='mx-auto max-w-3xl px-4 sm:px-6 xl:max-w-5xl xl:px-0'>
           <div className="flex h-screen flex-col justify-between">
-            <header className="flex items-center justify-between py-10">
-              <Header />
-              <div className="flex items-center text-base leading-5">
-                <div className="hidden sm:block">
-                  {headerNavLinks.map((link) => (
-                    <Link
-                      key={link.title}
-                      href={link.href}
-                      className="p-1 font-medium text-gray-900 dark:text-gray-100 sm:p-4"
-                    >
-                      {link.title}
-                    </Link>
-                  ))}
-                </div>
-              </div>
-            </header>
-            <main className="px-6">{children}</main>
+            <Header />
+            <main className="mb-auto">{children}</main>
           </div>
         </div>
-
         <Analytics />
       </body>
     </html>
