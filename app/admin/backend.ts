18c4531748226eae8ccdcba1e40af36fa431d9e4
Oleksandr Pavlov <olpavlov@deloitte.com>
2023-05-16 02:13:00 +0300

    Revert "up"
    This reverts commit bbc42feba1aee12b8423726a7b7aae620ebda3c5.

--- app/admin/backend.ts
+++ app/admin/backend.ts
@@ -1,34 +1,4 @@
-"use server";
-
 import { Octokit, App } from "octokit";
-
-export async function createNewPost(formData: any) {
-    const data = Object.fromEntries(formData);
-
-    const octokit = new Octokit({ auth: `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` });
- 
-   
-    let buff = Buffer.from(data.post);
-    let base64data = buff.toString('base64')
-
-
-    var resp = await octokit.request(`PUT /repos/pavlovtech/nextblog/contents/posts/${data.slug}.mdx`, {
-        owner: 'pavlovtech',
-        repo: 'NextBlog',
-        path: './posts',
-        message: 'my commit message',
-        committer: {
-          name: 'Alex Pavlov',
-          email: 'alexpppavlov93@gmail.com'
-        },
-        content: base64data,
-        headers: {
-          'X-GitHub-Api-Version': '2022-11-28'
-        }
-    });
-
-    console.log('git resp', resp);
-}
 
 export async function getAllPosts() {
     const octokit = new Octokit({ auth: `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` });
