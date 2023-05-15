afb2aa33777ac9e570e145f38b5d17d28ee5d0fa
Oleksandr Pavlov <olpavlov@deloitte.com>
2023-05-16 01:56:42 +0300

    test

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,5 +1,5 @@
 import { redirect } from "next/navigation";
-import { createNewPost, getAllPosts } from "./backend";
+import { getAllPosts } from "./backend";
 import Post from "./components/post";
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "configs/auth-options";
