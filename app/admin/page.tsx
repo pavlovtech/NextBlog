134cd1e8ceb61d0298d92ea32deebcdc064858cf
Oleksandr Pavlov <olpavlov@deloitte.com>
2023-05-15 16:06:30 +0300

    up

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,7 +1,7 @@
 "use server";
 
 import { redirect } from "next/navigation";
-import { createNewPost } from "./_actions";
+import { createNewPost, getAllPosts } from "./_actions";
 import Post from "./components/post";
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "configs/auth-options";
@@ -11,12 +11,17 @@ const Admin = async () => {
   const session = await getServerSession(authOptions);
   console.log(session);
 
+  const posts = await getAllPosts();
+
   if (!session) {
     redirect("/api/auth/signin?callbackUrl=/admin");
   }
 
   return (
     <>
+      {posts.map((post, idx) => (
+          <p key={idx}>{post.name}</p>
+        ))}
       <Post />
     </>
 
