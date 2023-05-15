bbc42feba1aee12b8423726a7b7aae620ebda3c5
Oleksandr Pavlov <olpavlov@deloitte.com>
2023-05-16 02:00:14 +0300

    up

--- app/admin/components/post.tsx
+++ app/admin/components/post.tsx
@@ -1,41 +1,11 @@
 import { Octokit, App } from "octokit";
 
-async function createNewPost(formData: any) {
-    "use server";
-    const data = Object.fromEntries(formData);
-
-    const octokit = new Octokit({ auth: `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` });
- 
-   
-    let buff = Buffer.from(data.post);
-    let base64data = buff.toString('base64')
-
-
-    var resp = await octokit.request(`PUT /repos/pavlovtech/nextblog/contents/posts/${data.slug}.mdx`, {
-        owner: 'pavlovtech',
-        repo: 'NextBlog',
-        path: './posts',
-        message: 'my commit message',
-        committer: {
-          name: 'Alex Pavlov',
-          email: 'alexpppavlov93@gmail.com'
-        },
-        content: base64data,
-        headers: {
-          'X-GitHub-Api-Version': '2022-11-28'
-        }
-    });
-
-    console.log('git resp', resp);
-}
-
-
 const Post = async({ slug } : {slug?: string}) => {
   
     return (
      <div className='container'>
       {/* @ts-expect-error Server Component */}
-        <form action={createNewPost} className='w-full max-w-sm'>
+        <form className='w-full max-w-sm'>
             <div className='mb-6 md:flex md:items-center'>
             <div className='md:w-1/3'>
             <label
