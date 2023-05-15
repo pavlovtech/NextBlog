8753fb145b305054abbf8ec25259a973c8bfe629
Oleksandr Pavlov <olpavlov@deloitte.com>
2023-05-16 02:27:36 +0300

    up

--- app/admin/backend.ts
+++ app/admin/backend.ts
@@ -1,16 +1,14 @@
 import { Octokit, App } from "octokit";
 
-export async function createNewPost(formData: any) {
-    const data = Object.fromEntries(formData);
-
+export async function createNewPost(slug: string, post: string) {
     const octokit = new Octokit({ auth: `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` });
  
    
-    let buff = Buffer.from(data.post);
+    let buff = Buffer.from(post);
     let base64data = buff.toString('base64')
 
 
-    var resp = await octokit.request(`PUT /repos/pavlovtech/nextblog/contents/posts/${data.slug}.mdx`, {
+    var resp = await octokit.request(`PUT /repos/pavlovtech/nextblog/contents/posts/${slug}.mdx`, {
         owner: 'pavlovtech',
         repo: 'NextBlog',
         path: './posts',
@@ -25,7 +23,7 @@ export async function createNewPost(formData: any) {
         }
     });
 
-    console.log('git resp', resp);
+    return resp;
 }
 
 export async function getAllPosts() {
---
+++ app/api/posts/route.ts
@@ -0,0 +1,8 @@
+import { createNewPost } from 'app/admin/backend';
+import { NextResponse } from 'next/server';
+
+export async function POST(request: Request) {
+    const req = await request.json();
+    const resp = await createNewPost(req.slug, req.post);
+    return NextResponse.json(resp);
+}
