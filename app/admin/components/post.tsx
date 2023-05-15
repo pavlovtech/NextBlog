ea5a75045bd9e3be41b859e3d3d4a800be034e82
Oleksandr Pavlov <olpavlov@deloitte.com>
2023-05-16 01:06:57 +0300

    Revert "up"
    This reverts commit b46c827d5d557a1e332b004f53b36b8464bc4cf6.

---
+++ app/admin/components/post.tsx
@@ -0,0 +1,66 @@
+"use server";
+
+import { createNewPost } from "../_actions";
+
+const Post = async({ slug } : {slug?: string}) => {
+    return (
+     <div className='container'>
+      {/* @ts-expect-error Server Component */}
+        <form action={createNewPost} className='w-full max-w-sm'>
+            <div className='mb-6 md:flex md:items-center'>
+            <div className='md:w-1/3'>
+            <label
+                className='mb-1 block pr-4 font-bold text-gray-500 md:mb-0 md:text-right'
+                htmlFor='slug'
+            >
+                Slug
+            </label>
+            </div>
+            <div className='md:w-2/3'>
+            <input
+                className='w-full appearance-none rounded border-2 border-gray-200 bg-gray-200 px-4 py-2 leading-tight text-gray-700 focus:border-purple-500 focus:bg-white focus:outline-none'
+                id='slug'
+                name='slug'
+                type='text'
+                required
+            />
+            </div>
+            </div>
+            
+            <div className='mb-6 md:flex md:items-center'>
+            <div className='md:w-1/3'>
+            <label
+                className='mb-1 block pr-4 font-bold text-gray-500 md:mb-0 md:text-right'
+                htmlFor='post'
+            >
+                Post
+            </label>
+            </div>
+            <div className='md:w-2/3'>
+            <textarea
+                className='w-full appearance-none rounded border-2 border-gray-200 bg-gray-200 px-4 py-2 leading-tight text-gray-700 focus:border-purple-500 focus:bg-white focus:outline-none'
+                id='post'
+                name='post'
+                rows={4} cols={40}
+                required
+            />
+            </div>
+            </div>
+          
+            <div className='md:flex md:items-center'>
+          <div className='md:w-1/3'></div>
+          <div className='md:w-2/3'>
+            <button
+              type='submit'
+              className=''
+            >
+              Submit
+            </button>
+          </div>
+        </div>
+        </form>
+     </div>
+    )
+  }
+  
+  export default Post
---
+++ app/admin/page.tsx
@@ -0,0 +1,34 @@
+import { redirect } from "next/navigation";
+import { getAllPosts } from "./_actions";
+import Post from "./components/post";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "configs/auth-options";
+import { LoginButton, LogoutButton } from "./components/buttons";
+import { Key } from "react";
+
+const Admin = async () => {
+  const session = await getServerSession(authOptions);
+  console.log(session);
+
+  const posts = await getAllPosts();
+
+  if (!session) {
+    redirect("/api/auth/signin?callbackUrl=/admin");
+  }
+
+  return (
+    <>
+      <div>
+        <LogoutButton />
+      </div>
+      {posts.map((post: { name: string }, idx: Key) => (
+          <p key={idx}>{post.name}</p>
+        ))}
+      {/* @ts-expect-error Server Component */}
+      <Post />
+    </>
+
+  )
+}
+
+export default Admin
