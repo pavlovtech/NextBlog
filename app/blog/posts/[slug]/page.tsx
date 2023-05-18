51eb73ff5ae37a991e02de82b5d800444d5b18a5
Oleksandr Pavlov <olpavlov@deloitte.com>
2023-05-18 18:39:38 +0300

    test

--- app/blog/posts/[slug]/page.tsx
+++ app/blog/posts/[slug]/page.tsx
@@ -2,8 +2,9 @@ import { format, parseISO } from 'date-fns'
 import { Post, allPosts } from 'contentlayer/generated'
 import { getMDXComponent } from 'next-contentlayer/hooks'
 import '../../../../styles/prism-atom-dark.css';
-import { headers } from 'next/headers';
 import { notFound } from 'next/navigation';
+import { Disqus } from 'app/components/comments/disqus';
+
 
 export const generateStaticParams = async () => allPosts.map((post) => ({ slug: post._raw.flattenedPath }))
 
@@ -25,16 +26,17 @@ const PostPage = ({ params }: { params: { slug: string } }) => {
   return (
     <div className='flex flex-col justify-center'>
       <div className="xl:divide-y xl:divide-gray-200 xl:dark:divide-gray-700">
-      <div className="mb-8 text-center">
-        <time dateTime={post.date} className="mb-1 text-xs text-gray-600">
-          {format(parseISO(post.date), 'LLLL d, yyyy')}
-        </time>
-        <h1 className='text-3xl font-bold'>{post.title}</h1>
+        <div className="mb-8 text-center">
+          <time dateTime={post.date} className="mb-1 text-xs text-gray-600">
+            {format(parseISO(post.date), 'LLLL d, yyyy')}
+          </time>
+          <h1 className='text-3xl font-bold'>{post.title}</h1>
+        </div>
+        <div className='max-w-max prose prose-dark prose-invert prose-lg'>
+          <Content />
+        </div>
       </div>
-      <div className='max-w-max prose prose-dark prose-invert prose-lg'>
-        <Content />
-      </div>
-    </div>
+      <Disqus post={post}></Disqus>
     </div>
   )
 }
