88e56b7ac93b7ff70f885466d360a31a1339cf66
Oleksandr Pavlov <olpavlov@deloitte.com>
2023-05-18 23:16:23 +0300

    up

--- app/blog/layout.tsx
+++ app/blog/layout.tsx
@@ -3,27 +3,19 @@ import '../../styles/globals.css';
 import { Analytics } from '@vercel/analytics/react';
 
 import { Inter } from 'next/font/google'
-import Link from 'next/link';
-import { getServerSession } from 'next-auth';
-import { authOptions } from 'configs/auth-options';
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Alex Pavlov - blog',
   description: 'Alex Pavlov - blog about programming'
 }
 
-export default async function RootLayout({ children }: { children: React.ReactNode }) {
-  const session = await getServerSession(authOptions)
+const headerNavLinks = [
+  { href: '/blog', title: 'Blog' },
+  { href: '/blog/tags', title: 'Tags' },
+  { href: '/blog/projects', title: 'Projects' }
+]
 
-  const headerNavLinks = [
-    { href: '/blog', title: 'Blog' },
-    { href: '/blog/tags', title: 'Tags' },
-    { href: '/blog/projects', title: 'Projects' },
-    ...session ? [{ href: '/admin', title: 'Admin' }] : [],
-  ]
-
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <div className='mx-auto max-w-3xl px-4 sm:px-6 xl:max-w-5xl xl:px-0'>
