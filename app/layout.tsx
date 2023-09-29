f0762beaba325c92391cde05f0f2b47ec1cfee78
Oleksandr Pavlov <olpavlov@deloitte.com>
2023-09-29 15:26:46 +0300

    up

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import '../styles/globals.css';
 
 import { Inter } from 'next/font/google'
-import { NextAuthProvider } from './admin/components/provider';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -12,9 +11,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <link rel="icon" type="image/x-icon" href="/favicon.png" />
       </head>
       <body className={`${inter.className} bg-white text-black antialiased dark:bg-background-color dark:text-white`}>
-        <NextAuthProvider>
           {children}
-        </NextAuthProvider>
       </body>
     </html>
   )
